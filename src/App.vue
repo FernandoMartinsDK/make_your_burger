29aff87c147f1b503d821fbe808ebcc880e12865
Fernando Diego Martins Coelho <fernandomartinsdk@gmail.com>
2023-05-23 22:27:38 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Navbar/>
+  <Footer></Footer>
+  <router-view/>
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue';
+  import Footer from './components/Footer.vue';
+
+  export default{
+    components:{
+      Navbar,
+      Footer
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">
+    <h1>HOME1</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
