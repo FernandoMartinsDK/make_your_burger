0dc6e31a6fa37e306945e6440d1680deecb949cd
Fernando Diego Martins Coelho <fernandomartinsdk@gmail.com>
2023-05-27 20:26:04 -0300

    add funcão delete

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -27,7 +27,7 @@
                     <option value="">Selecione</option>
                     <option v-for="s in status" :key="s.id" :value="s.tipo"  :selected="burger.status == s.tipo">{{ s.tipo }}</option>
                 </select>
-                <button class="delete-btn">Cancelar</button>
+                <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
             </div>
         </div>
     </div>
@@ -57,7 +57,15 @@
                 this.status = data;
                 console.log(this.status)
 
+            },
+            async deleteBurger(id){
+                const req = await fetch(`http://localhost:3000/burgers/${id}`,{
+                    method:"DELETE"
+                });
+                const res = await req.json();
+                this.getPedidos();
             }
+
         },
         mounted(){
             this.getPedidos();
